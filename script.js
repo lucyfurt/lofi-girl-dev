3c4a816b3186a1b9c858ca880c4c2fa544577981
lucyfurt <lucianafurtado10@gmail.com>
2024-02-01 17:12:31 -0300

    pomodoro timer

--- script.js
+++ script.js
@@ -88,3 +88,74 @@ audio.play(); // Iniciar a reprodução automaticamente
 
 // Adicionar evento de clique para o botão play/pause
 playPauseBtn.addEventListener('click', togglePlayPause);
+
+"use strict";
+
+let hour = 0;
+let minute = 0;
+let second = 0;
+let millisecond = 0;
+
+let cron;
+
+document.form_main.start.onclick = () => start();
+document.form_main.pause.onclick = () => pause();
+document.form_main.reset.onclick = () => reset();
+
+function start() {
+    pause();
+    cron = setInterval(() => { timer(); }, 10);
+  }
+  
+  function pause() {
+    clearInterval(cron);
+  }
+  
+  function reset() {
+    hour = 0;
+    minute = 0;
+    second = 0;
+    millisecond = 0;
+    document.getElementById('hour').innerText = '00';
+    document.getElementById('minute').innerText = '00';
+    document.getElementById('second').innerText = '00';
+    document.getElementById('millisecond').innerText = '000';
+  }
+
+  function timer() {
+    if ((millisecond += 10) == 1000) {
+      millisecond = 0;
+      second++;
+    }
+    if (second == 60) {
+      second = 0;
+      minute++;
+    }
+    if (minute == 60) {
+      minute = 0;
+      hour++;
+    }
+    document.getElementById('hour').innerText = returnData(hour);
+    document.getElementById('minute').innerText = returnData(minute);
+    document.getElementById('second').innerText = returnData(second);
+    document.getElementById('millisecond').innerText = returnData(millisecond);
+  }
+  
+  function returnData(input) {
+    return input > 10 ? input : `0${input}`
+  }
+
+  
+document.querySelector('a[href="#"] img[alt="pomodoro"]').addEventListener('click', function () {
+    
+    var timerFormWrapper = document.getElementById('timerFormWrapper');
+
+    // Toggle the display property of the form wrapper
+    if (timerFormWrapper.style.display === 'none') {
+        timerFormWrapper.style.display = 'block';
+    } else {
+        timerFormWrapper.style.display = 'none';
+    }
+});
+
+
